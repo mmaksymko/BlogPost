ecceee1c4d7973f227cee780ea41ffa0250c5654
maksymkomyna@gmail.com <maksymkomyna@gmail.com>
2024-05-24 02:51:41 +0300

    [refactor] config server directory renamed

---
+++ blog-post-backend/config-server/src/test/java/dev/mmaksymko/configserver/ConfigurationServerApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.mmaksymko.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigurationServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
