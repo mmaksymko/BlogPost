b04b73844d85a058d6dff55377ec54e13ef3ebf4
maksymkomyna@gmail.com <maksymkomyna@gmail.com>
2024-05-24 03:20:29 +0300

    [feat] add eureka discovery server

---
+++ blog-post-backend/discovery-server/src/test/java/dev/mmaksymko/discovery/DiscoveryServerApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.mmaksymko.discovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
