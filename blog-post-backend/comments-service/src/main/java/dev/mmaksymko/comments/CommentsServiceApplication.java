378651e71b600546419ed27c6c474f4610f323e4
maksymkomyna@gmail.com <maksymkomyna@gmail.com>
2024-05-24 17:40:13 +0300

    [fix] spring boot 3.3.0 Serializing 'PageImpl instances as-is is not supported' warning fix

--- blog-post-backend/comments-service/src/main/java/dev/mmaksymko/comments/CommentsServiceApplication.java
+++ blog-post-backend/comments-service/src/main/java/dev/mmaksymko/comments/CommentsServiceApplication.java
@@ -2,8 +2,10 @@ package dev.mmaksymko.comments;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
 
 @SpringBootApplication
+@EnableSpringDataWebSupport(pageSerializationMode = EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO)
 public class CommentsServiceApplication {
 
 	public static void main(String[] args) {
