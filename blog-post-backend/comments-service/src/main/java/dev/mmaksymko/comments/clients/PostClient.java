fdd9614c49315783ff83aa9df2879de532369f41
maksymkomyna@gmail.com <maksymkomyna@gmail.com>
2024-05-24 19:14:04 +0300

    [feat] create feign client

---
+++ blog-post-backend/comments-service/src/main/java/dev/mmaksymko/comments/clients/PostClient.java
@@ -0,0 +1,12 @@
+package dev.mmaksymko.comments.clients;
+
+import dev.mmaksymko.comments.models.Post;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@FeignClient(name = "blog-post-service")
+public interface PostClient {
+    @GetMapping("/posts/{id}/")
+    Post getPost(@PathVariable Long id);
+}
