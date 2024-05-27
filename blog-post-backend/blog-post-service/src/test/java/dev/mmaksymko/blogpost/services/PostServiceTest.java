008a6d3d263503ba72f231ceb0496df7abaf4310
maksymkomyna@gmail.com <maksymkomyna@gmail.com>
2024-05-27 22:28:46 +0300

    [ref] group related tests to nested classes

--- blog-post-backend/blog-post-service/src/test/java/dev/mmaksymko/blogpost/services/PostServiceTest.java
+++ blog-post-backend/blog-post-service/src/test/java/dev/mmaksymko/blogpost/services/PostServiceTest.java
@@ -8,6 +8,7 @@ import dev.mmaksymko.blogpost.models.Post;
 import dev.mmaksymko.blogpost.repositories.PostRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
@@ -36,135 +37,129 @@ public class PostServiceTest {
     @InjectMocks
     private PostService postService;
 
+    private Post post;
+    private PostResponse postResponse;
+    private PostRequest postRequest;
+    private PostUpdateRequest postUpdateRequest;
+    private Pageable pageable;
+
     @BeforeEach
     public void setup() {
+
         MockitoAnnotations.openMocks(this);
-    }
-
-    @Test
-    @DisplayName("GetPosts: returns a page of posts")
-    public void testGetPosts() {
-        Post post = Post.builder().title("Test Title").content("Test Content").build();
-        PostResponse postResponse = new PostResponse(1L, "Test Title", "Test Content", 1L, null);
-        Pageable pageable = Pageable.unpaged();
-
-        when(postRepository.findAll(pageable)).thenReturn(new PageImpl<>(Collections.singletonList(post)));
-        when(postMapper.toResponse(post)).thenReturn(postResponse);
-
-        Page<PostResponse> result = postService.getPosts(pageable);
-
-        assertEquals(1, result.getContent().size());
-        assertEquals(postResponse, result.getContent().get(0));
-        verify(postRepository).findAll(pageable);
-        verify(postMapper).toResponse(post);
-    }
-
-    @Test
-    @DisplayName("GetPosts: returns empty page when none inserted")
-    public void testGetPostsWhenEmpty() {
-        Pageable pageable = Pageable.unpaged();
-
-        when(postRepository.findAll(pageable)).thenReturn(new PageImpl<>(Collections.emptyList()));
-
-        Page<PostResponse> result = postService.getPosts(pageable);
-
-        assertEquals(0, result.getContent().size());
-        verify(postRepository).findAll(pageable);
-    }
-
-    @ParameterizedTest
-    @DisplayName("GetPost: returns the post specified by id")
-    @ValueSource(longs = {1L, 2L, 3L})
-    public void testGetPostWhenCorrectId(Long id) {
-        Post post = Post.builder().title("Test Title").content("Test Content").build();
-        PostResponse postResponse = new PostResponse(id, "Test Title", "Test Content", 1L, null);
-
-        when(postRepository.findById(id)).thenReturn(Optional.of(post));
-        when(postMapper.toResponse(post)).thenReturn(postResponse);
-
-        PostResponse result = postService.getPost(id);
-
-        assertEquals(postResponse, result);
-        verify(postRepository).findById(id);
-        verify(postMapper).toResponse(post);
-    }
-
-    @Test
-    @DisplayName("GetPost: throws NoSuchElementException when id is inexistent")
-    public void testGetPostWhenInexistentIdThrowsNoSuchElement() {
-        Long insertId = 1L;
-        Long testId = 2L;
-        Post post = Post.builder().title("Test Title").content("Test Content").build();
-        PostResponse postResponse = new PostResponse(insertId, "Test Title", "Test Content", 1L, null);
-
-        when(postRepository.findById(any(Long.class))).thenAnswer(invocation -> {
-            Long argument = invocation.getArgument(0);
-            if (argument.equals(insertId)) {
-                return Optional.of(post);
-            } else {
-                return Optional.empty();
-            }
-        });
-
-        when(postMapper.toResponse(post)).thenReturn(postResponse);
-
-        assertThrows(NoSuchElementException.class, () -> postService.getPost(2L));
-        verify(postRepository).findById(testId);
+        post = Post.builder().title("Test Title").content("Test Content").build();
+        postResponse = new PostResponse(1L, "Test Title", "Test Content", 1L, null);
+        postRequest = new PostRequest("Test Title", "Test Content", 1L);
+        postUpdateRequest = new PostUpdateRequest("Updated Title", "Updated Content");
+        pageable = Pageable.unpaged();
     }
 
 
-    @Test
-    @DisplayName("AddPost: added post is returned")
-    public void testAddPost() {
-        PostRequest postRequest = new PostRequest("Test Title", "Test Content", 1L);
-        Post post = Post.builder().title("Test Title").content("Test Content").build();
-        PostResponse postResponse = new PostResponse(1L, "Test Title", "Test Content", 1L, null);
+    @Nested
+    class GetPostsTests {
+        @Test
+        @DisplayName("returns a page of posts")
+        public void whenPostsExist() {
+            when(postRepository.findAll(pageable)).thenReturn(new PageImpl<>(Collections.singletonList(post)));
+            when(postMapper.toResponse(post)).thenReturn(postResponse);
 
-        when(postMapper.toEntity(any(PostRequest.class))).thenReturn(post);
-        when(postRepository.save(any(Post.class))).thenReturn(post);
-        when(postMapper.toResponse(post)).thenReturn(postResponse);
+            Page<PostResponse> result = postService.getPosts(pageable);
 
-        PostResponse result = postService.addPost(postRequest);
+            assertEquals(1, result.getContent().size());
+            assertEquals(postResponse, result.getContent().get(0));
+            verify(postRepository).findAll(pageable);
+            verify(postMapper).toResponse(post);
+        }
 
-        assertEquals(postResponse, result);
+        @Test
+        @DisplayName("returns empty page when none inserted")
+        public void whenNoPostsExist() {
+            when(postRepository.findAll(pageable)).thenReturn(new PageImpl<>(Collections.emptyList()));
 
-        verify(postMapper).toEntity(postRequest);
-        verify(postRepository).save(post);
-        verify(postMapper).toResponse(post);
+            Page<PostResponse> result = postService.getPosts(pageable);
+
+            assertEquals(0, result.getContent().size());
+            verify(postRepository).findAll(pageable);
+        }
     }
 
-    @Test
-    @DisplayName("UpdatePost: updated post is returned")
-    public void testUpdatePost() {
-        Long id = 1L;
-        PostUpdateRequest postUpdateRequest = new PostUpdateRequest("Updated Title", "Updated Content");
-        Post post = Post.builder().title("Test Title").content("Test Content").build();
-        PostResponse postResponse = new PostResponse(id, "Updated Title", "Updated Content", 1L, null);
+    @Nested
+    class GetPostTests {
+        @ParameterizedTest
+        @DisplayName("returns the post specified by id")
+        @ValueSource(longs = {1L, 2L, 3L})
+        public void whenIdExists(Long id) {
+            when(postRepository.findById(id)).thenReturn(Optional.of(post));
+            when(postMapper.toResponse(post)).thenReturn(postResponse);
 
-        when(postRepository.findById(any(Long.class))).thenReturn(Optional.of(post));
-        when(postRepository.save(any(Post.class))).thenReturn(post);
-        when(postMapper.toResponse(post)).thenReturn(postResponse);
+            PostResponse result = postService.getPost(id);
 
-        PostResponse result = postService.updatePost(id, postUpdateRequest);
+            assertEquals(postResponse, result);
+            verify(postRepository).findById(id);
+            verify(postMapper).toResponse(post);
+        }
 
-        assertEquals(postResponse, result);
-        verify(postRepository).findById(id);
-        verify(postRepository).save(post);
-        verify(postMapper).toResponse(post);
+        @Test
+        @DisplayName("throws NoSuchElementException when id is inexistent")
+        public void whenIdDoesNotExist() {
+            Long testId = 2L;
+
+            when(postRepository.findById(any(Long.class))).thenReturn(Optional.empty());
+
+            assertThrows(NoSuchElementException.class, () -> postService.getPost(testId));
+            verify(postRepository).findById(testId);
+        }
     }
 
-    @Test
-    @DisplayName("UpdatePost: throws exception when post does not exist")
-    public void testUpdatePostWhenInexistentIdThrowsNoSuchElement() {
-        Long id = 1L;
-        PostUpdateRequest postUpdateRequest = new PostUpdateRequest("Updated Title", "Updated Content");
+    @Nested
+    class AddPostTests {
+        @Test
+        @DisplayName("added post is returned")
+        public void whenPostIsValid() {
+            when(postMapper.toEntity(any(PostRequest.class))).thenReturn(post);
+            when(postRepository.save(any(Post.class))).thenReturn(post);
+            when(postMapper.toResponse(post)).thenReturn(postResponse);
 
-        when(postRepository.findById(any(Long.class))).thenReturn(Optional.empty());
+            PostResponse result = postService.addPost(postRequest);
 
-        assertThrows(NoSuchElementException.class, () -> postService.updatePost(id, postUpdateRequest));
-
-        verify(postRepository).findById(id);
+            assertEquals(postResponse, result);
+            verify(postMapper).toEntity(postRequest);
+            verify(postRepository).save(post);
+            verify(postMapper).toResponse(post);
+        }
     }
+
+    @Nested
+    class UpdatePostTests {
+        @Test
+        @DisplayName("updated post is returned")
+        public void whenPostExists() {
+            Long id = 1L;
+
+            when(postRepository.findById(any(Long.class))).thenReturn(Optional.of(post));
+            when(postRepository.save(any(Post.class))).thenReturn(post);
+            when(postMapper.toResponse(post)).thenReturn(postResponse);
+
+            PostResponse result = postService.updatePost(id, postUpdateRequest);
+
+            assertEquals(postResponse, result);
+            verify(postRepository).findById(id);
+            verify(postRepository).save(post);
+            verify(postMapper).toResponse(post);
+        }
+
+        @Test
+        @DisplayName("throws exception when post does not exist")
+        public void whenPostDoesNotExist() {
+            Long id = 1L;
+
+            when(postRepository.findById(any(Long.class))).thenReturn(Optional.empty());
+
+            assertThrows(NoSuchElementException.class, () -> postService.updatePost(id, postUpdateRequest));
+            verify(postRepository).findById(id);
+        }
+    }
+
     @Test
     @DisplayName("DeletePost: post is deleted")
     public void testDeletePost() {
