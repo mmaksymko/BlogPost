789dd0304273e8f0502fe54b9d1d24d7968bfb68
maksymkomyna@gmail.com <maksymkomyna@gmail.com>
2024-05-30 05:35:25 +0300

    [feat] create reactions service

---
+++ blog-post-backend/reactions-service/src/main/java/dev/mmaksymko/reactions/configs/CustomErrorDecoder.java
@@ -0,0 +1,40 @@
+package dev.mmaksymko.reactions.configs;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import dev.mmaksymko.reactions.dto.ErrorResponse;
+import dev.mmaksymko.reactions.configs.exceptions.ExternalApiClientException;
+import dev.mmaksymko.reactions.configs.exceptions.ExternalApiServerException;
+import feign.Response;
+import feign.codec.ErrorDecoder;
+import org.springframework.http.HttpStatus;
+
+import java.io.IOException;
+
+public class CustomErrorDecoder implements ErrorDecoder {
+
+    @Override
+    public Exception decode(String methodKey, Response response) {
+        String url = response.request().url();
+        HttpStatus status = HttpStatus.valueOf(response.status());
+        Response.Body body = response.body();
+
+        String errorMessage = getErrorMessage(url, status, body);
+        if (status.is5xxServerError()) {
+            return new ExternalApiServerException("Server error: " + errorMessage);
+        } else {
+            return new ExternalApiClientException("Client error: " + errorMessage);
+        }
+    }
+
+    private String getErrorMessage(String url, HttpStatus status, Response.Body body) {
+        String error;
+        try {
+            ObjectMapper objectMapper = new ObjectMapper();
+            error = objectMapper.readValue(body.asInputStream(), ErrorResponse.class).error();
+        } catch (IOException e) {
+            error = status.name();
+        }
+
+        return error + " at " + url;
+    }
+}
