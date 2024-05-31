910bd1f982762daa85270d3424e815e59c66361b
maksymkomyna@gmail.com <maksymkomyna@gmail.com>
2024-05-31 18:19:47 +0300

    [feat] add check for kafka availability it is used to add resilience to the microservice in case of kafka server being down

---
+++ blog-post-backend/comments-service/src/main/java/dev/mmaksymko/comments/configs/KafkaAvailabilityManager.java
@@ -0,0 +1,53 @@
+package dev.mmaksymko.comments.configs;
+
+import org.apache.kafka.clients.admin.AdminClient;
+import org.apache.kafka.common.KafkaException;
+import org.apache.kafka.common.Node;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.core.KafkaAdmin;
+
+import java.util.Collection;
+import java.util.Properties;
+import java.util.concurrent.*;
+
+@Configuration
+public class KafkaAvailabilityManager {
+    private final Properties properties;
+    private AdminClient adminClient;
+
+    public KafkaAvailabilityManager(KafkaAdmin kafkaAdmin) {
+        properties = new Properties();
+        properties.putAll(kafkaAdmin.getConfigurationProperties());
+    }
+
+    public AdminClient getAdminClient() throws KafkaException {
+        if (adminClient != null) {
+            return adminClient;
+        }
+        FutureTask<AdminClient> futureTask = new FutureTask<>(() -> AdminClient.create(properties));
+        try (ExecutorService executorService = Executors.newSingleThreadExecutor()) {
+            executorService.execute(futureTask);
+            AdminClient adminClient = futureTask.get(1, TimeUnit.SECONDS);
+            if (adminClient == null) {
+                throw new KafkaException();
+            }
+            this.adminClient = adminClient;
+            return adminClient;
+        } catch (TimeoutException e) {
+            futureTask.cancel(true);
+            throw new KafkaException();
+        } catch (Exception e) {
+            throw new KafkaException();
+        }
+    }
+
+    public boolean isAvailable() {
+        try {
+            AdminClient admin = getAdminClient();
+            Collection<Node> nodes = admin.describeCluster().nodes().get();
+            return nodes != null && !nodes.isEmpty();
+        } catch (Exception e) {
+            return false;
+        }
+    }
+}
