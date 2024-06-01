358d1aedd03f4725618c84b4cdf20502f4593bed
maksymkomyna@gmail.com <maksymkomyna@gmail.com>
2024-06-02 02:46:45 +0300

    [feat] enable async

--- blog-post-backend/comments-service/src/main/java/dev/mmaksymko/comments/CommentsServiceApplication.java
+++ blog-post-backend/comments-service/src/main/java/dev/mmaksymko/comments/CommentsServiceApplication.java
@@ -4,9 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.data.web.config.EnableSpringDataWebSupport;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
 @EnableFeignClients
+@EnableAsync
 @EnableSpringDataWebSupport(pageSerializationMode = EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO)
 public class CommentsServiceApplication {
 
