4df97ee0fb423212e683aafc697cc1e20c076ec3
maksymkomyna@gmail.com <maksymkomyna@gmail.com>
2024-06-02 02:51:39 +0300

    [feat] annotate redis cached entities

--- blog-post-backend/reactions-service/src/main/java/dev/mmaksymko/reactions/models/Comment.java
+++ blog-post-backend/reactions-service/src/main/java/dev/mmaksymko/reactions/models/Comment.java
@@ -4,6 +4,8 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.redis.core.RedisHash;
 
 import java.time.LocalDateTime;
 
@@ -11,7 +13,9 @@ import java.time.LocalDateTime;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@RedisHash("Comment")
 public class Comment {
+    @Id
     private Long commentId;
     private Long postId;
     private Long userId;
