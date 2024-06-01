c7bb0d475b9fd47008e7c188d21667d4b40f32e5
maksymkomyna@gmail.com <maksymkomyna@gmail.com>
2024-06-02 02:54:11 +0300

    [feat] reflect events on cache

--- blog-post-backend/reactions-service/src/main/java/dev/mmaksymko/reactions/services/kafka/CommentConsumer.java
+++ blog-post-backend/reactions-service/src/main/java/dev/mmaksymko/reactions/services/kafka/CommentConsumer.java
@@ -1,14 +1,25 @@
 package dev.mmaksymko.reactions.services.kafka;
 
 import dev.mmaksymko.reactions.dto.kafka.CommentEvent;
+import dev.mmaksymko.reactions.dto.kafka.EventType;
+import dev.mmaksymko.reactions.services.redis.RedisCommentService;
+import lombok.AllArgsConstructor;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Service;
 
 @Service
+@AllArgsConstructor
 public class CommentConsumer {
-    @KafkaListener(id = "reactions-comment-event-listener", topics = "comment-event")
-    public void postEventListener(@Payload CommentEvent postEvent) {
-        System.out.println(postEvent);
+    private final RedisCommentService commentService;
+
+    @KafkaListener(id = "reactions-comment-event-listener", topics = "comment-event", autoStartup = "false")
+    public void postEventListener(@Payload CommentEvent commentEvent) {
+        switch(commentEvent.getEventType()) {
+            case EventType.CREATED -> commentService.saveComment(commentEvent);
+            case EventType.UPDATED -> commentService.updateComment(commentEvent);
+            case EventType.DELETED -> commentService.deleteComment(commentEvent.getCommentId());
+            default -> throw new IllegalArgumentException("Unexpected event type: " + commentEvent.getEventType());
+        }
     }
 }
--- blog-post-backend/reactions-service/src/main/java/dev/mmaksymko/reactions/services/kafka/PostConsumer.java
+++ blog-post-backend/reactions-service/src/main/java/dev/mmaksymko/reactions/services/kafka/PostConsumer.java
@@ -1,14 +1,25 @@
 package dev.mmaksymko.reactions.services.kafka;
 
+import dev.mmaksymko.reactions.dto.kafka.EventType;
 import dev.mmaksymko.reactions.dto.kafka.PostEvent;
+import dev.mmaksymko.reactions.services.redis.RedisPostService;
+import lombok.AllArgsConstructor;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Service;
 
 @Service
+@AllArgsConstructor
 public class PostConsumer {
-    @KafkaListener(id = "reactions-post-event-listener", topics = "post-event")
+    private final RedisPostService postService;
+
+    @KafkaListener(id = "reactions-post-event-listener", topics = "post-event", autoStartup = "false")
     public void postEventListener(@Payload PostEvent postEvent) {
-        System.out.println(postEvent);
+        switch(postEvent.getEventType()) {
+            case EventType.CREATED -> postService.savePost(postEvent);
+            case EventType.UPDATED -> postService.updatePost(postEvent);
+            case EventType.DELETED -> postService.deletePost(postEvent.getId());
+            default -> throw new IllegalArgumentException("Unexpected event type: " + postEvent.getEventType());
+        }
     }
 }
