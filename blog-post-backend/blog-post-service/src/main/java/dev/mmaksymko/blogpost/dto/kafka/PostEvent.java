d05046d35325ff18be0ac5490abd6b77faea5520
maksymkomyna@gmail.com <maksymkomyna@gmail.com>
2024-06-02 02:38:15 +0300

    [fix] add lombok data annotation

--- blog-post-backend/blog-post-service/src/main/java/dev/mmaksymko/blogpost/dto/kafka/PostEvent.java
+++ blog-post-backend/blog-post-service/src/main/java/dev/mmaksymko/blogpost/dto/kafka/PostEvent.java
@@ -1,10 +1,11 @@
 package dev.mmaksymko.blogpost.dto.kafka;
 
-
 import dev.mmaksymko.blogpost.dto.PostResponse;
 import lombok.EqualsAndHashCode;
+import lombok.Data;
 
 @EqualsAndHashCode(callSuper = true)
+@Data
 public class PostEvent extends PostResponse {
     private final EventType eventType;
 
