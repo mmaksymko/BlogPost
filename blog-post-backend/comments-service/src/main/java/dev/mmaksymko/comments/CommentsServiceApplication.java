6eb348f201a28db81ce2aec92ab8ca6dbef8de2c
maksymkomyna@gmail.com <maksymkomyna@gmail.com>
2024-06-02 02:47:31 +0300

    [feat] enable scheduling Used in kafka configuration

--- blog-post-backend/comments-service/src/main/java/dev/mmaksymko/comments/CommentsServiceApplication.java
+++ blog-post-backend/comments-service/src/main/java/dev/mmaksymko/comments/CommentsServiceApplication.java
@@ -5,10 +5,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.data.web.config.EnableSpringDataWebSupport;
 import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableFeignClients
 @EnableAsync
+@EnableScheduling
 @EnableSpringDataWebSupport(pageSerializationMode = EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO)
 public class CommentsServiceApplication {
 
