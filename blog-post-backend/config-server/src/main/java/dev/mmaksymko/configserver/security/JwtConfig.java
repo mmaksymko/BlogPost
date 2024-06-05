8dfabc244817f96ed10e2da29d3421163c62c7d0
maksymkomyna@gmail.com <maksymkomyna@gmail.com>
2024-06-06 02:36:22 +0300

    [feat] configure propagation of authorization from gateway server to microservices

---
+++ blog-post-backend/config-server/src/main/java/dev/mmaksymko/configserver/security/JwtConfig.java
@@ -0,0 +1,19 @@
+package dev.mmaksymko.configserver.security;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.JwtDecoders;
+
+@Configuration
+public class JwtConfig {
+
+    @Value("${spring.security.oauth2.client.provider.google.issuer-uri}")
+    String issuerUri;
+
+    @Bean
+    JwtDecoder jwtDecoder() {
+        return JwtDecoders.fromOidcIssuerLocation(issuerUri);
+    }
+}
---
+++ blog-post-backend/discovery-server/src/main/java/dev/mmaksymko/discovery/security/SecurityConfig.java
@@ -0,0 +1,29 @@
+package dev.mmaksymko.discovery.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableMethodSecurity
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/actuator/**", "/error", "/webjars/**").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .oauth2ResourceServer(oauth2ResourceServer ->
+                        oauth2ResourceServer.jwt(jwt ->
+                                jwt.jwtAuthenticationConverter(new JwtAuthenticationConverter())
+                        )
+                ).build();
+    }
+}
