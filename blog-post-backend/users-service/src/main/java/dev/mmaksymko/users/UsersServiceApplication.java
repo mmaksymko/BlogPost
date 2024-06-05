9e249d99e8112d775ec5c5d643815d67c8976eba
maksymkomyna@gmail.com <maksymkomyna@gmail.com>
2024-06-06 02:32:07 +0300

    [fix] enable feign clients

--- blog-post-backend/users-service/src/main/java/dev/mmaksymko/users/UsersServiceApplication.java
+++ blog-post-backend/users-service/src/main/java/dev/mmaksymko/users/UsersServiceApplication.java
@@ -2,11 +2,13 @@ package dev.mmaksymko.users;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.data.web.config.EnableSpringDataWebSupport;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
 @EnableAsync
+@EnableFeignClients
 @EnableSpringDataWebSupport(pageSerializationMode = EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO)
 public class UsersServiceApplication {
 
