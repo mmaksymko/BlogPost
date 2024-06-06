1f7d88fd8d1065d38a881ffe41362382c37e7614
maksymkomyna@gmail.com <maksymkomyna@gmail.com>
2024-06-06 17:33:45 +0300

    [feat] configure logout

--- blog-post-backend/gateway-server/src/main/java/dev/mmaksymko/gateway/handlers/OAuth2LogoutSuccessHandler.java
+++ blog-post-backend/gateway-server/src/main/java/dev/mmaksymko/gateway/handlers/OAuth2LogoutSuccessHandler.java
@@ -1,45 +1,18 @@
 package dev.mmaksymko.gateway.handlers;
 
-import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpStatusCode;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.oauth2.client.ReactiveOAuth2AuthorizedClientService;
-import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
 import org.springframework.security.web.server.WebFilterExchange;
 import org.springframework.security.web.server.authentication.logout.ServerLogoutSuccessHandler;
-import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
 
 @Configuration
-@RequiredArgsConstructor
 public class OAuth2LogoutSuccessHandler implements ServerLogoutSuccessHandler {
-    private final WebClient webClient;
-    private final ReactiveOAuth2AuthorizedClientService authorizedClientService;
 
     @Override
-    public Mono<Void> onLogoutSuccess(WebFilterExchange exchange, Authentication authentication) {
-        if (authentication instanceof OAuth2AuthenticationToken oauthToken) {
-            return authorizedClientService.loadAuthorizedClient(
-                    oauthToken.getAuthorizedClientRegistrationId(),
-                    oauthToken.getName()
-            ).flatMap(authorizedClient -> {
-                if (authorizedClient != null) {
-                    String accessToken = authorizedClient.getAccessToken().getTokenValue();
-                    return revokeGoogleToken(accessToken); // Revoke token before clearing session
-                }
-                return Mono.empty();
-            }).then();
-        }
-        return Mono.empty();
-    }
-
-    private Mono<Void> revokeGoogleToken(String accessToken) {
-        String revokeTokenEndpoint = "https://oauth2.googleapis.com/revoke?token=" + accessToken;
-        return webClient.post()
-                .uri(revokeTokenEndpoint)
-                .retrieve()
-                .onStatus(HttpStatusCode::isError, response -> Mono.error(new RuntimeException("Failed to revoke token")))
-                .bodyToMono(Void.class);
+    public Mono<Void> onLogoutSuccess(WebFilterExchange webFilterExchange, Authentication authentication) {
+        webFilterExchange.getExchange().getResponse().setStatusCode(HttpStatus.OK);
+        return webFilterExchange.getExchange().getResponse().setComplete();
     }
 }
