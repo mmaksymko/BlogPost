6e2a0a03b20f4147928726fe0d0c4c065aaaca33
maksymkomyna@gmail.com <maksymkomyna@gmail.com>
2024-06-06 12:26:56 +0300

    [fix] enable scheduling

--- blog-post-backend/gateway-server/src/main/java/dev/mmaksymko/gateway/GatewayServerApplication.java
+++ blog-post-backend/gateway-server/src/main/java/dev/mmaksymko/gateway/GatewayServerApplication.java
@@ -7,10 +7,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableRedisRepositories
 @EnableConfigurationProperties({GeoLite2Properties.class, FrontEndProperties.class, RedisProperties.class})
+@EnableScheduling
 public class GatewayServerApplication {
 
 	public static void main(String[] args) {
