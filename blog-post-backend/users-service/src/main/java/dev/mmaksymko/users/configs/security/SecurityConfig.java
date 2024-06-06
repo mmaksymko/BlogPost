1047687bfd4f455b8412400359323c6d04301d17
maksymkomyna@gmail.com <maksymkomyna@gmail.com>
2024-06-06 16:04:25 +0300

    [feat] expose get user endpoints

--- blog-post-backend/users-service/src/main/java/dev/mmaksymko/users/configs/security/SecurityConfig.java
+++ blog-post-backend/users-service/src/main/java/dev/mmaksymko/users/configs/security/SecurityConfig.java
@@ -2,6 +2,7 @@ package dev.mmaksymko.users.configs.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -12,12 +13,12 @@ import org.springframework.security.web.SecurityFilterChain;
 @EnableMethodSecurity
 @EnableWebSecurity
 public class SecurityConfig {
-
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/actuator/**", "/error", "/webjars/**").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/users/{id}", "/users/email/{email}/").permitAll()
                         .anyRequest().authenticated()
                 )
                 .oauth2ResourceServer(oauth2ResourceServer ->
