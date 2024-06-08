a70babfe481c5901796f4b31624bad22c006c660
maksymkomyna@gmail.com <maksymkomyna@gmail.com>
2024-06-08 04:29:44 +0300

    [feat] add footer add header to pages

--- blog-post-frontend/src/App.tsx
+++ blog-post-frontend/src/App.tsx
@@ -3,6 +3,9 @@ import './App.css';
 
 import Home from "./pages/Home";
 
+import Header from './components/Header';
+import Footer from './components/Footer';
+
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { SnackBarContext, defaultSnackBar, SnackBarState } from './contexts/SnackBarContext';
 import { Alert, Snackbar } from '@mui/material';
@@ -15,13 +18,14 @@ function App() {
     }
     setSnackBar(prev => ({ ...prev, open: false }));
   };
-
   return (
     <SnackBarContext.Provider value={{ ...snack, setSnackBar }}>
       <BrowserRouter>
+        <Header />
         <Routes>
           <Route path="/" element={<Home />} />
         </Routes>
+        <Footer />
       </BrowserRouter >
       <Snackbar anchorOrigin={{ vertical: 'bottom', horizontal: 'center' }} open={snack.open} autoHideDuration={6000} onClose={handleClose}>
         <Alert onClose={handleClose} severity={snack.severity} variant="outlined" sx={{ background: "var(--primary-color)", width: '125%', maxWidth: "75vw" }}>
